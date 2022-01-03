ad42d3cd0ad45875b9d1bad1627ad20d79774de1
lm93547 <lucasmartin045@gmail.com>
2022-01-03 11:16:36 +0800

    removed config info

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
 export const config = {
-  REACT_APP_MAPBOX:
-    'pk.eyJ1IjoibGptMjIzIiwiYSI6ImNrcnM3YXBncTMzd3oyb3J2dXJhMm8xaTIifQ.RKHZqM9awnh9snvL6Jy7MA',
-  homeLocation: {
-    longitude: -7.797060314155499,
-    latitude: 55.12385298871404,
-    zoom: 5,
-  },
-  siteTitle: 'Earth Explorer',
-  apiUrl: 'http://localhost:8000',
+
 };
